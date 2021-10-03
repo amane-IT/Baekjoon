6ce9d5897735fcb04a406545bcfe5fad97b18895
이주은 <48676089+amane-IT@users.noreply.github.com>
2021-10-04 02:16:17 +0900

    Create 두 수의 합.java

---
+++ 210904/두 수의 합.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        
+        for(int i = 0; i < n; i++) {
+        	arr[i] = sc.nextInt();
+        }
+        
+        int target = sc.nextInt();
+        
+        Arrays.sort(arr);
+        
+        int start = 0;
+        int end = n - 1;
+        int cnt = 0;
+        
+        while(start < end) {
+        	int sum = arr[start] + arr[end];
+        	
+        	if(sum == target)
+        		cnt++;
+        	
+        	if (sum <= target)
+        		start++;
+        	else
+        		end--;
+        }
+        
+        System.out.println(cnt);
+    }
+}
