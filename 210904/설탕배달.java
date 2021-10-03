c9d194a6907cd400880a3fc4f91a646d6a6dce68
이주은 <48676089+amane-IT@users.noreply.github.com>
2021-10-04 02:12:06 +0900

    Create 설탕배달.java

---
+++ 210904/설탕배달.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        
+        int kg = sc.nextInt();
+        
+        if(kg % 5 == 0) {
+        	System.out.println(kg / 5);
+        	return;
+        	
+        } else {
+        	int quotient = kg / 5;
+        	for(int i = quotient; i > 0; i--) {
+        		int tmp = kg - (i * 5);
+        		if(tmp % 3 == 0) {
+        			System.out.println(i + (tmp / 3));
+        			return;
+        		}
+        	}
+        }
+        
+        if(kg % 3 == 0) {
+        	System.out.println(kg / 3);
+        }
+        else
+        	System.out.println(-1);
+        
+    }
+}
