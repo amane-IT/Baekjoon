a4dbe7b30b63b4444403cedbadb1c817cdd06c55
이주은 <48676089+amane-IT@users.noreply.github.com>
2021-10-05 23:59:19 +0900

    Create 배수와 약수.java

---
+++ 210914/배수와 약수.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+class Main {
+	
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        
+        while(true) {
+        	int num1 = sc.nextInt();
+            int num2 = sc.nextInt();
+            
+            if(num1 == 0 && num2 == 0)
+            	break;
+            
+            if(num1 < num2) {
+            	if(num2 % num1 == 0)
+            		System.out.println("factor");
+            	else
+            		System.out.println("neither");
+            } else {
+            	if(num1 % num2 == 0)
+            		System.out.println("multiple");
+            	else
+            		System.out.println("neither");
+            }
+            
+            
+        }
+        
+	}
+}
