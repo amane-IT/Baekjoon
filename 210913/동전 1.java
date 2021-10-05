3d881767948c5ab3e6597a0e2a2eeddef9f16b25
이주은 <48676089+amane-IT@users.noreply.github.com>
2021-10-05 23:44:15 +0900

    Create 동전 1.java

---
+++ 210913/동전 1.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+class Main {
+	
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+        
+        int[] arr = new int[n + 1];
+        int[] dp = new int[k + 1];
+        dp[0] = 1;
+        
+        for(int i = 1; i <= n; i++) {
+        	arr[i] = sc.nextInt();
+        	for(int j = arr[i]; j <= k; j++) {
+        		dp[j] += dp[j - arr[i]];
+        	}
+        }
+        
+        System.out.println(dp[k]);
+	}
+}
