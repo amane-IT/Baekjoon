9e260263bf6e47e193dc631005cb9448deba7d78
이주은 <48676089+amane-IT@users.noreply.github.com>
2021-10-05 23:59:59 +0900

    Create 약수.java

---
+++ 210914/약수.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+class Main {
+	
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        
+        for(int i = 0; i < n; i++) {
+        	arr[i] = sc.nextInt();
+        }
+        
+        Arrays.sort(arr);
+        
+        System.out.println(arr[0] * arr[n - 1]);
+	}
+}
