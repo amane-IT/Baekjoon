c0cfb6f8dbecae908304ca025e303f842867649d
이주은 <48676089+amane-IT@users.noreply.github.com>
2021-10-05 23:43:30 +0900

    Create 최소, 최대.java

---
+++ 210913/최소, 최대.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+class Main {
+	
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        
+        int n = sc.nextInt();
+        int max = Integer.MIN_VALUE;
+        int min = Integer.MAX_VALUE;
+        
+        for(int i = 0; i < n; i++) {
+        	int num = sc.nextInt();
+        	max = Math.max(max, num);
+        	min = Math.min(min, num);
+        }
+        
+        System.out.println(min + " " + max);
+	}
+}
