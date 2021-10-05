bdbc4eae46e64fb6c3e313cc7e40305373464048
이주은 <48676089+amane-IT@users.noreply.github.com>
2021-10-05 23:42:47 +0900

    Create 영화감독 숌.java

---
+++ 210913/영화감독 숌.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+class Main {
+	
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        
+        int n = sc.nextInt();
+        int count = 1;
+        int num = 666;
+        
+        while(count != n) {
+        	num++;
+        	if(Integer.toString(num).contains("666"))
+        		count++;
+        }
+        
+        System.out.println(num);
+	}
+}
