bfcd47d5dc5f05b110c36313af92448e0707ad1d
이주은 <48676089+amane-IT@users.noreply.github.com>
2021-10-07 23:52:21 +0900

    Create 이항 계수 1.java

---
+++ 210917/이항 계수 1.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+class Main {
+	
+	/*
+	 * 이항계수 = n! / ((n - k)! * k!)
+	 */
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+        
+        System.out.println(factorial(n) / (factorial(n - k) * factorial(k)));
+    }
+    
+    static int factorial(int n) {
+    	if(n <= 1)
+    		return 1;
+    	
+    	return n * factorial(n - 1);
+    }
+}
