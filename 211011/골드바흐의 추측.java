28a9a7a2429274061c5bba1afbb3778ffb973fce
이주은 <48676089+amane-IT@users.noreply.github.com>
2021-10-12 00:49:28 +0900

    Create 골드바흐의 추측.java

---
+++ 211011/골드바흐의 추측.java
@@ -0,0 +1,38 @@
+import java.util.*;
+
+class Main {
+	
+	public static void main(String[] args) {
+    	Scanner sc = new Scanner(System.in);
+    	
+    	int t = sc.nextInt();
+    	
+    	boolean[] check = new boolean[10001];
+    	Arrays.fill(check, true);
+    	
+    	for(int i = 2; i <= 10000; i++) {
+    		for(int j = i * 2; j <= 10000; j += i) {
+    			if(!check[j])
+    				continue;
+    			check[j] = false;
+    		}
+    	}
+    	
+    	while(t --> 0) {
+    		int num = sc.nextInt();
+    		boolean flag = false;
+    		int n1 = 0;
+    		int n2 = 0;
+    		
+    		for(int i = 2; i <= num/2; i++) {
+    			if(check[i] && check[num - i]) {
+    				n1 = i;
+    				n2 = num - i;
+    			}
+    		}
+    		
+    		System.out.println(n1 + " " + n2);
+    	}
+    	
+  }
+}
