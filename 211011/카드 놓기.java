7a440b191631db14273372ab2ca8f78839586dd3
이주은 <48676089+amane-IT@users.noreply.github.com>
2021-10-12 00:46:47 +0900

    Create 카드 놓기.java

---
+++ 211011/카드 놓기.java
@@ -0,0 +1,42 @@
+import java.util.*;
+
+class Main {
+	static int n, k;
+	static int[] arr;
+	static boolean[] check;
+	static List<String> list = new ArrayList<>();
+	
+	public static void main(String[] args) {
+    	Scanner sc = new Scanner(System.in);
+    	n = sc.nextInt();
+    	k = sc.nextInt();
+
+    	arr = new int[n];
+    	check = new boolean[n];
+    	
+    	for(int i = 0; i < n; i++) {
+    		arr[i] = sc.nextInt();
+    	}
+    	
+    	perm(0, "");
+    	
+    	System.out.println(list.size());
+    	
+	}
+	
+	static void perm(int depth, String s) {
+		if(depth == k) {
+			if(!list.contains(s))
+				list.add(s);
+			return;
+		}
+		
+		for(int i = 0; i < n; i++) {
+			if(!check[i]) {
+				check[i] = true;
+				perm(depth + 1, s + arr[i]);
+				check[i] = false;
+			}
+		}
+	}
+}
