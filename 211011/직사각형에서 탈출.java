d4f591f86a3ec081176d350bf7f0c0c752d7906b
이주은 <48676089+amane-IT@users.noreply.github.com>
2021-10-12 00:51:47 +0900

    Create 직사각형에서 탈출.java

---
+++ 211011/직사각형에서 탈출.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+class Main {
+	
+	public static void main(String[] args) {
+    	Scanner sc = new Scanner(System.in);
+    	
+    	int x = sc.nextInt();
+    	int y = sc.nextInt();
+    	int w = sc.nextInt();
+    	int h = sc.nextInt();
+    	
+    	int x_min = Math.min(x, w - x);
+    	int y_min = Math.min(y, h - y);
+    	
+    	System.out.println(Math.min(x_min, y_min));
+    	
+	}
+}
