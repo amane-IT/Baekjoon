b51035ccba212b47f3745c2f9dc4d0215b2b2988
이주은 <48676089+amane-IT@users.noreply.github.com>
2021-10-12 00:48:52 +0900

    Create 베르트랑 공준.java

---
+++ 211011/베르트랑 공준.java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+class Main {
+	
+	public static void main(String[] args) {
+    	Scanner sc = new Scanner(System.in);
+    	
+    	while(true) {
+    		int num = sc.nextInt();
+    		if(num == 0)
+    			break;
+    		
+    		int cnt = 0;
+    		
+    		for(int i = num + 1; i <= num * 2; i++) {
+    			if(find(i))
+    				cnt++;
+    		}
+    		
+    		System.out.print(cnt + "\n");
+    	}
+    	
+	}
+	
+	static boolean find(int num) {
+		if(num == 1)
+			return false;
+		
+		for(int i = 2; i <= Math.sqrt((double) num); i++) {
+			if(num % i == 0)
+				return false;
+		}
+		
+		return true;
+	}
+}
