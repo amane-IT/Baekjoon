2fe4a97d8ba4968d75c03c92639920ebd24458d5
이주은 <48676089+amane-IT@users.noreply.github.com>
2021-10-12 00:48:25 +0900

    Create 소수 구하기.java

---
+++ 211011/소수 구하기.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+class Main {
+	
+	public static void main(String[] args) {
+    	Scanner sc = new Scanner(System.in);
+    	int n = sc.nextInt();
+    	int m = sc.nextInt();
+    	
+    	List<Integer> list = new ArrayList<>();
+    	
+    	for(int i = n; i <= m; i++) {
+    		if(find(i))
+    			System.out.print(i + "\n");
+    	}
+	}
+	
+	static boolean find(int num) {
+		if(num == 1)
+			return false;
+		
+		for(int i = 2; i <= Math.sqrt((double) num); i++) {
+			if(num % i == 0)
+				return false;
+		}
+		
+		return true;
+	}
+}
