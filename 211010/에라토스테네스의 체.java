f800c5d964cb3074d479415480123ed375e5b216
이주은 <48676089+amane-IT@users.noreply.github.com>
2021-10-12 00:53:09 +0900

    Create 에라토스테네스의 체.java

---
+++ 211010/에라토스테네스의 체.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+class Main {
+	static boolean[] check;
+	
+	public static void main(String[] args) {
+    	Scanner sc = new Scanner(System.in);
+    	
+    	int n = sc.nextInt();
+    	int k = sc.nextInt();
+    	int cnt = 0;
+    	int flag = 0;
+    	
+    	check = new boolean[n + 1];
+    	
+    	for(int i = 2; i <= n; i++) {
+    		for(int j = i; j <= n; j += i) {
+    			if(!check[j]) {
+    				cnt++;
+    				check[j] = true;
+    			}
+    			
+    			if(cnt == k) {
+    				System.out.println(j);
+    				System.exit(0);
+    			}
+    		}
+    	}
+	}
+}
