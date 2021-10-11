e42f90bb72620ca1c81fb62def46c2bb6b863c70
이주은 <48676089+amane-IT@users.noreply.github.com>
2021-10-12 00:48:05 +0900

    Create 소인수분해.java

---
+++ 211011/소인수분해.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+class Main {
+	
+	public static void main(String[] args) {
+    	Scanner sc = new Scanner(System.in);
+    	int n = sc.nextInt();
+    	int num = 2;
+    	List<Integer> list = new ArrayList<>();
+    	
+    	while(n > 1) {
+    		if(n % num == 0) {
+    			list.add(num);
+    			n /= num;
+    		}
+    		else {
+    			num++;
+    		}
+    	}
+    	
+    	for(int i = 0; i < list.size(); i++) {
+    		System.out.print(list.get(i) + "\n");
+    	}
+    	
+	}
+}
