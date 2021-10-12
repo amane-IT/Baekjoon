999a6cbe9827b06ddcb1b7421a808b88a5835aa5
이주은 <48676089+amane-IT@users.noreply.github.com>
2021-10-12 23:51:46 +0900

    Create 가장 긴 증가하는 부분 수열 2.java

---
+++ 211012/가장 긴 증가하는 부분 수열 2.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+class Main {
+	
+	public static void main(String[] args) {
+    	Scanner sc = new Scanner(System.in);
+    	
+    	int n = sc.nextInt();
+    	List<Integer> list = new ArrayList<>();
+    	list.add(0);
+    	
+    	for(int i = 0; i < n; i++) {
+    		int tmp = sc.nextInt();
+    		
+    		if(list.get(list.size() - 1) < tmp)
+    			list.add(tmp);
+    		else {
+    			int left = 1;
+    			int right = list.size() - 1;
+    			
+    			while(left < right) {
+    				int mid = (left + right) / 2;
+    				if(list.get(mid) < tmp)
+    					left = mid + 1;
+    				else
+    					right = mid;
+    			}
+    			list.set(right, tmp);
+    		}
+    	}
+    	
+    	System.out.print(list.size() - 1);
+    	
+	}
+}
