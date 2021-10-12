58adbed63ba64d1fb551c8ff5604794cb0989cac
이주은 <48676089+amane-IT@users.noreply.github.com>
2021-10-12 23:52:03 +0900

    Create 곱셈.java

---
+++ 211012/곱셈.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+class Main {
+	
+	public static void main(String[] args) {
+    	Scanner sc = new Scanner(System.in);
+    	
+    	long a = sc.nextLong();
+    	long b = sc.nextLong();
+    	long c = sc.nextLong();
+    	
+    	
+    	System.out.print(pow(a, b, c) % c);
+    	
+	}
+	
+	static long pow(long a, long b, long c) {
+		if(b == 0)
+			return 1;
+		else if (b == 1)
+			return a;
+		else if (b % 2 == 0) {
+			long n = pow(a, b / 2, c) % c;
+			return (n * n) % c;
+		} else {
+			long n = pow(a, b / 2, c) % c;
+			return (((n * n) % c) * a) % c;
+		}
+	}
+}
