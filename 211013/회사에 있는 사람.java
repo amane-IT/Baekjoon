681164706db9831d0b48141db507dc2fe7065a55
이주은 <48676089+amane-IT@users.noreply.github.com>
2021-10-13 01:35:21 +0900

    Create 회사에 있는 사람.java

---
+++ 211013/회사에 있는 사람.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+class Main {
+	
+	public static void main(String[] args) {
+    	Scanner sc = new Scanner(System.in);
+    	
+    	int n = sc.nextInt();
+    	Map<String, Integer> map = new HashMap<>();
+    	
+    	for(int i = 0; i < n; i++) {
+    		String name = sc.next();
+    		String inout = sc.next();
+    		
+    		if(map.containsKey(name))
+    			map.remove(name);
+    		else
+    			map.put(name, 1);
+    	}
+    	
+    	List<String> list = new ArrayList<>();
+
+    	for(String s : map.keySet()) {
+    		list.add(s);
+    	}
+    	
+    	Collections.sort(list, Collections.reverseOrder());
+    	
+    	for(String s : list) {
+    		System.out.print(s + "\n");
+    	}
+	}
+}
