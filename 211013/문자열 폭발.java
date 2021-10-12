a679a557502de81c6b30bc1194d9657d019fe5f8
이주은 <48676089+amane-IT@users.noreply.github.com>
2021-10-13 01:34:59 +0900

    Create 문자열 폭발.java

---
+++ 211013/문자열 폭발.java
@@ -0,0 +1,38 @@
+import java.util.*;
+
+class Main {
+	
+	public static void main(String[] args) {
+    	Scanner sc = new Scanner(System.in);
+    	
+    	String s = sc.next();
+    	String bomb = sc.next();
+    	Stack<Character> stack = new Stack<>();
+    	
+    	for(int i = 0; i < s.length(); i++) {
+    		stack.push(s.charAt(i));
+    		
+    		if(stack.size() >= bomb.length()) {
+    			boolean flag = true;
+    			for(int j = 0; j < bomb.length(); j++) {
+    				if(stack.get(stack.size() - bomb.length() + j) != bomb.charAt(j)) {
+    					flag = false;
+    					break;
+    				}
+    			}
+    			
+    			if(flag)
+    				for(int j = 0; j < bomb.length(); j++) {
+    					stack.pop();
+    				}
+    		}
+    	}
+    	
+    	StringBuilder sb = new StringBuilder();
+    	for(char c : stack) {
+    		sb.append(c);
+    	}
+    	
+    	System.out.println(sb.length() > 0 ? sb.toString() : "FRULA");
+	}
+}
