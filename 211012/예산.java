0cea3b32f19aace99bc02be848ba7e782c681fc0
이주은 <48676089+amane-IT@users.noreply.github.com>
2021-10-12 23:51:16 +0900

    Create 예산.java

---
+++ 211012/예산.java
@@ -0,0 +1,41 @@
+import java.util.*;
+
+class Main {
+	
+	public static void main(String[] args) {
+    	Scanner sc = new Scanner(System.in);
+    	
+    	int n = sc.nextInt();
+    	int[] arr = new int[n];
+    	long re = 0;
+    	
+    	for(int i = 0; i < n; i++) {
+    		arr[i] = sc.nextInt();
+    	}
+    	
+    	Arrays.sort(arr);
+    	
+    	long m = sc.nextLong();
+    	long left = 0, right = arr[n - 1];
+    	
+    	while(left <= right) {
+    		long mid = (left + right) / 2;
+    		long sum = 0;
+    		
+    		for(int money : arr) {
+    			if(money >= mid)
+    				sum += mid;
+    			else
+    				sum += money;
+    		}
+    		
+    		if(sum > m) {
+    			right = mid - 1;
+    		} else {
+    			left = mid + 1;
+    			re = Math.max(re, mid);
+    		}
+    	}
+    	System.out.print(re);
+	}
+}
