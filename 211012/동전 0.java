81c23ebd45f9a16e1ae29c497691652b5c14588b
이주은 <48676089+amane-IT@users.noreply.github.com>
2021-10-12 23:50:45 +0900

    Create 동전 0.java

---
+++ 211012/동전 0.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+class Main {
+	
+	public static void main(String[] args) {
+    	Scanner sc = new Scanner(System.in);
+    	
+    	int n = sc.nextInt();
+    	int k = sc.nextInt();
+    	int[] coin = new int[n];
+    	
+    	for(int i = 0; i < n; i++) {
+    		coin[i] = sc.nextInt();
+    	}
+    	
+    	int cnt = 0;
+    	
+    	for(int i = n - 1; i >= 0; i--) {
+    		if(coin[i] <= k) {
+    			cnt += k / coin[i];
+    			k = k % coin[i];
+    		}
+    	}
+    	
+    	System.out.println(cnt);
+	}
+}
