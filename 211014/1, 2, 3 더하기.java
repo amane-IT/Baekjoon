727f2e8a392587341467c53626bbd54d7c50895e
이주은 <48676089+amane-IT@users.noreply.github.com>
2021-10-14 23:59:23 +0900

    Create 1, 2, 3 더하기.java

---
+++ 211014/1, 2, 3 더하기.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+class Main {
+	
+	public static void main(String[] args) {
+    	Scanner sc = new Scanner(System.in);
+    	
+    	int t = sc.nextInt();
+    	int[] dp = new int[11];
+    	
+    	dp[1] = 1;
+    	dp[2] = 2;
+    	dp[3] = 4;
+    	
+    	for(int i = 4; i < 11; i++) {
+    		dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3];
+    	}
+    	
+    	while(t --> 0) {
+    		int num = sc.nextInt();
+    		
+    		System.out.println(dp[num]);
+    	}
+    	
+	}
+}
