5213de6f9a957d4b1d3418891487a3ea64e37ebb
이주은 <48676089+amane-IT@users.noreply.github.com>
2021-10-15 02:46:50 +0900

    Create 에너지 모으기.java

---
+++ 211015/에너지 모으기.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+class Main {
+	static int max = 0;
+	
+	public static void main(String[] args) {
+    	Scanner sc = new Scanner(System.in);
+    	
+    	int n = sc.nextInt();
+    	List<Integer> list = new ArrayList<>();
+    	
+    	for(int i = 0; i < n; i++) {
+    		list.add(sc.nextInt());
+    	}
+    	
+    	dfs(list, 0);
+    	
+    	System.out.print(max);
+    	
+	}
+	
+	static void dfs(List<Integer> list, int sum) {
+		if(list.size() == 2) {
+			if(max < sum)
+				max = sum;
+			return;
+		}
+		
+		for(int i = 1; i < list.size() - 1; i++) {
+			int tmp = list.get(i);
+			int num = list.get(i - 1) * list.get(i + 1);
+			list.remove(i);
+			dfs(list, sum + num);
+			list.add(i, tmp);
+		}
+	}
+}
