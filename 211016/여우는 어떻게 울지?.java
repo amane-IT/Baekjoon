b5f6c35d6bf2a984397e9baf9d38827943437dbe
이주은 <48676089+amane-IT@users.noreply.github.com>
2021-10-16 23:35:30 +0900

    Create 여우는 어떻게 울지?.java

---
+++ 211016/여우는 어떻게 울지?.java
@@ -0,0 +1,41 @@
+import java.util.*;
+
+class Main {
+	public static void main(String[] args) {
+    	Scanner sc = new Scanner(System.in);
+    	
+    	int n = sc.nextInt();
+    	sc.nextLine();
+    	
+    	for(int i = 0; i < n; i++){
+    		String[] sound = sc.nextLine().split(" ");
+    		List<String> list = new ArrayList<>();
+    		
+    		for(int j = 0; j < sound.length; j++)
+    			list.add(sound[j]);
+    		
+    		while(true) {
+    			String ani = sc.nextLine();
+    			
+    			if(ani.equals("what does the fox say?"))
+    				break;
+    			
+    			String[] animal = ani.split(" ");
+    			
+    			for(int j = 0; j < list.size(); j++) {
+    				if(list.get(j).equals(animal[2])) {
+    					list.set(j, "-");
+    				}
+    			}
+    		}
+    		
+    		String result = "";
+    		for(int j = 0; j < list.size(); j++) {
+    			if(list.get(j).equals("-"))
+    				continue;
+    			result += list.get(j) + " ";
+    		}
+    		System.out.println(result);
+    	}
+	}
+}
