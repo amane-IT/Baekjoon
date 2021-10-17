18a8d83a60aed02b4957815bb1db8dda6af70556
이주은 <48676089+amane-IT@users.noreply.github.com>
2021-10-17 21:47:37 +0900

    Create 로프.java

---
+++ 211017/로프.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+class Main {
+	
+	public static void main(String[] args) {
+    	Scanner sc = new Scanner(System.in);
+    	
+    	int n = sc.nextInt();
+    	int[] arr = new int[n];
+    	
+    	for(int i = 0; i < n; i++) {
+    		arr[i] = sc.nextInt();
+    	}
+    	
+    	Arrays.sort(arr);
+    	
+    	long max = 0;
+    	for(int i = n - 1; i >= 0; i--) {
+    		arr[i] = arr[i] * (n - i);
+    		max = Math.max(max, arr[i]);
+    	}
+    	
+    	System.out.println(max);
+	}
+}
