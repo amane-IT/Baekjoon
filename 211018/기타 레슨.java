8cabfdc340b44cac00f0dcfab2b33ceb3c9cd371
이주은 <48676089+amane-IT@users.noreply.github.com>
2021-10-18 20:42:42 +0900

    Create 기타 레슨.java

---
+++ 211018/기타 레슨.java
@@ -0,0 +1,43 @@
+import java.util.*;
+
+class Main {
+	
+	public static void main(String[] args) {
+    	Scanner sc = new Scanner(System.in);
+    	
+    	int n = sc.nextInt();
+    	int m = sc.nextInt();
+    	int[] arr = new int[n];
+    	int left = 0;
+    	int right = 0;
+    	
+    	for(int i = 0; i < n; i++) {
+    		arr[i] = sc.nextInt();
+    		right += arr[i];
+    		left = Math.max(left, arr[i]);
+    	}
+    	
+    	while(left <= right) {
+    		int mid = (left + right) / 2;
+    		int sum = 0;
+    		int cnt = 0;
+    		
+    		for(int i = 0; i < n; i++) {
+    			if(sum + arr[i] > mid) {
+    				sum = 0;
+    				cnt++;
+    			}
+    			sum += arr[i];
+    		}
+    		
+    		if(sum != 0)
+    			cnt++;
+    		if(cnt <= m)
+    			right = mid - 1;
+    		else
+    			left = mid + 1;
+    	}
+    	
+    	System.out.println(left);
+	}
+}
