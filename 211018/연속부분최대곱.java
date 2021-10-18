03fec28cb17aed78aa03ef2d022b8390be87e771
이주은 <48676089+amane-IT@users.noreply.github.com>
2021-10-18 20:43:37 +0900

    Create 연속부분최대곱.java

---
+++ 211018/연속부분최대곱.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+class Main {
+	
+	public static void main(String[] args) {
+    	Scanner sc = new Scanner(System.in);
+    	
+    	int n = sc.nextInt();
+    	double[] arr = new double[n];
+    	double ans = 0, tmp =  1;
+    	
+    	for(int i = 0; i < n; i++) {
+    		arr[i] = sc.nextDouble();
+    	}
+    	
+    	for(int i = 0; i < n; i++) {
+    		tmp = 1;
+    		for(int j = i; j < n; j++) {
+    			tmp *= arr[j];
+    			ans = Math.max(ans, tmp);
+    		}
+    	}
+    	System.out.printf("%.3f", ans);   	
+	}
+}
