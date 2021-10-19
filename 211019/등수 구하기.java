846ed9010c0cfc38cfec9064f01f10dd73c832ee
이주은 <48676089+amane-IT@users.noreply.github.com>
2021-10-19 22:33:43 +0900

    Create 등수 구하기.java

---
+++ 211019/등수 구하기.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+class Main {
+	
+	public static void main(String[] args) {
+    	Scanner sc = new Scanner(System.in);
+    	
+    	int n = sc.nextInt();
+    	long newP = sc.nextLong();
+    	int p = sc.nextInt();
+    	List<Integer> list = new ArrayList<>();
+    	
+    	for(int i = 0; i < n; i++) {
+    		list.add(sc.nextInt());
+    	}
+    	
+    	Collections.sort(list, Collections.reverseOrder());
+    	int rank = 1;
+    	
+    	if(n == p && newP <= list.get(list.size() - 1))
+    		rank = -1;
+    	else {
+    		for(int i = 0; i < n; i++) {
+        		if(list.get(i) > newP) {
+        			rank++;
+        		} else {
+        			break;
+        		}
+        	}
+    	}
+    	
+    	System.out.println(rank);
+    	
+	}
+}
