3816c9b383013f9b571b13a47c2db133a328e247
이주은 <48676089+amane-IT@users.noreply.github.com>
2021-10-19 23:20:48 +0900

    Create 신입사원.java

---
+++ 211019/신입사원.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+class Main {
+	
+	public static void main(String[] args) {
+    	Scanner sc = new Scanner(System.in);
+    	
+    	int t = sc.nextInt();
+    	
+    	while(t --> 0) {
+    		int n = sc.nextInt();
+    		int[] arr = new int[n + 1];
+    		int cnt = 1;
+    		
+    		for(int i = 0; i < n; i++) {
+    			arr[sc.nextInt()] = sc.nextInt();
+    		}
+    		
+    		int num = arr[1];
+    		
+    		for(int i = 2; i <= n; i++) {
+    			if(num > arr[i]) {
+    				cnt++;
+    				num = arr[i];
+    			}
+    		}
+    		
+    		System.out.println(cnt);
+    	}
+	}
+}
