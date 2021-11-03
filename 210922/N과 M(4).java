c0b85d2b8e948a99531b17423337df895ace77ee
이주은 <48676089+amane-IT@users.noreply.github.com>
2021-11-04 00:56:12 +0900

    Create N과 M(4).java

---
+++ 210922/N과 M(4).java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+class Main {
+	
+	static int n, k;
+	static int[] arr;
+    public static void main(String[] args) {
+    	
+    	Scanner sc = new Scanner(System.in);
+    	
+    	n = sc.nextInt();
+    	k = sc.nextInt();
+    	arr = new int[k];
+    	
+    	dfs(1, 0);
+    }
+    
+    static void dfs(int idx, int depth) {
+    	if(depth == k) {
+    		for(int i = 0; i < k; i++) {
+    			System.out.print(arr[i] + " ");
+    		}
+    		
+    		System.out.println();
+    		return;
+    	}
+    	
+    	for(int i = idx; i <= n; i++) {
+    		arr[depth] = i;
+    		dfs(i, depth + 1);
+    	}
+    }
+}
