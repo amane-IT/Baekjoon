7db1789c59578dfae64b298b344158b534dd2d68
이주은 <48676089+amane-IT@users.noreply.github.com>
2021-11-04 00:55:04 +0900

    Create 구간 합 구하기 5(시간 초과).java

---
+++ 210922/구간 합 구하기 5(시간 초과).java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+class Main {
+	
+    public static void main(String[] args) {
+    	
+    	Scanner sc = new Scanner(System.in);
+    	int n = sc.nextInt();
+    	int m = sc.nextInt();
+    	int[][] map = new int[n][n];
+    	
+    	for(int i = 0; i < n; i++) {
+    		for(int j = 0; j < n; j++) {
+    			map[i][j] = sc.nextInt();
+    		}
+    	}
+    	
+    	
+    	while(m --> 0) {
+    		int x1 = sc.nextInt() - 1;
+    		int y1 = sc.nextInt() - 1;
+    		int x2 = sc.nextInt() - 1;
+    		int y2 = sc.nextInt() - 1;
+    		
+    		int result = 0;
+    		
+    		for(int i = x1; i <= x2; i++) {
+    			int sum = 0;
+    			for(int j = y1; j <= y2; j++) {
+    				sum += map[i][j];
+    			}
+    			result += sum;
+    		}
+    		System.out.println(result);
+    	}
+    }
+}
