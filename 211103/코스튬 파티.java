3fa5aa369bda8a32e0763400971309dc2f16e72d
이주은 <48676089+amane-IT@users.noreply.github.com>
2021-11-04 00:50:09 +0900

    Create 코스튬 파티.java

---
+++ 211103/코스튬 파티.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+class Main {
+	public static void main(String[] args) {
+    	
+    	Scanner sc = new Scanner(System.in);
+    	
+    	int n = sc.nextInt();
+    	int s = sc.nextInt();
+    	int[] arr = new int[n];
+    	
+    	for(int i = 0; i < n; i++) {
+    		arr[i] = sc.nextInt();
+    	}
+    	
+    	Arrays.sort(arr);
+    	int cnt = 0;
+    	
+    	for(int i = 0; i < n - 1; i++) {
+    		for(int j = i + 1; j < n; j++) {
+    			if(arr[i] + arr[j] > s || i == j)
+    				continue;
+    			cnt++;
+    			
+    		}
+    	}
+    	
+    	System.out.println(cnt);
+    }
+}
