9cc5a2285110e31b390b36058975679f7e13c910
이주은 <48676089+amane-IT@users.noreply.github.com>
2021-11-04 23:28:10 +0900

    Create 제일 작은 수 제거하기.java

---
+++ 211104/제일 작은 수 제거하기.java
@@ -0,0 +1,30 @@
+import java.util.List;
+import java.util.ArrayList;
+
+class Solution {
+    public int[] solution(int[] arr) {
+        int[] answer = new int[arr.length - 1];
+        List<Integer> list = new ArrayList<>();
+        
+        if(arr.length == 1)
+            list.add(-1);
+        
+        else{
+            int idx = 0;
+            
+            for(int i = 1; i < arr.length; i++){
+                if(arr[idx] > arr[i]){
+                    idx = i;
+                }
+            }
+            
+            for(int i = 0; i < arr.length; i++){
+                if(i == idx)
+                    continue;
+                list.add(arr[i]);
+            }
+        }
+        answer = list.stream().mapToInt(i->i).toArray();
+        return answer;
+    }
+}
