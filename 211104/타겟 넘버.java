cd79c0586a101829a8b494f3269cf0b343886b1c
이주은 <48676089+amane-IT@users.noreply.github.com>
2021-11-04 23:50:38 +0900

    Create 타겟 넘버.java

---
+++ 211104/타겟 넘버.java
@@ -0,0 +1,30 @@
+class Solution {
+    static int answer = 0;
+    public int solution(int[] numbers, int target) {
+        
+        dfs(0, target, numbers);
+        
+        return answer;
+    }
+    
+    static void dfs(int idx, int target, int[] numbers){
+        if(idx == numbers.length){
+            int sum = 0;
+            
+            for(int i = 0; i < numbers.length; i++){
+                sum += numbers[i];
+            }
+            
+            if(sum == target)
+                answer++;
+            
+            return;
+        }
+        
+        numbers[idx] *= 1;
+        dfs(idx + 1, target, numbers);
+        
+        numbers[idx] *= -1;
+        dfs(idx + 1, target, numbers);
+    }
+}
