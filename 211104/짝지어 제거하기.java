39c0469695c4b2f43d4737b4925a4bb0592a79d3
이주은 <48676089+amane-IT@users.noreply.github.com>
2021-11-04 23:58:00 +0900

    Create 짝지어 제거하기.java

---
+++ 211104/짝지어 제거하기.java
@@ -0,0 +1,21 @@
+import java.util.Stack;
+
+class Solution {
+    public int solution(String s) {
+        Stack<Character> stack = new Stack<>();
+		
+		for(int i = 0; i < s.length(); i++) {
+			char ch = s.charAt(i);
+			if(stack.isEmpty() || stack.peek() != ch)
+				stack.push(ch);
+			
+			else
+				stack.pop();
+		}
+		
+		if(stack.size() == 0)
+			return 1;
+		else
+			return 0;
+    }
+}
