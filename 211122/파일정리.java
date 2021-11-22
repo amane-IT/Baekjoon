2ed30f4384db1eb57b6a0221e0205e178f36261b
이주은 <48676089+amane-IT@users.noreply.github.com>
2021-11-22 21:45:34 +0900

    Create 파일정리.java

---
+++ 211122/파일정리.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+class Main {
+	public static void main(String[] args) {
+    	Scanner sc = new Scanner(System.in);
+    	Map<String, Integer> map = new HashMap<>();
+    	int n = sc.nextInt();
+    	
+    	while(n --> 0) {
+    		String s = sc.next();
+    		String[] tmp = s.split("\\.");
+    		
+    		map.put(tmp[1], map.getOrDefault(tmp[1], 0) + 1);
+    	}
+    	
+    	Object[] mapKey = map.keySet().toArray();
+    	Arrays.sort(mapKey);
+    	
+    	for(Object s : mapKey) {
+    		System.out.println(s + " " + map.get(s));
+    	}
+	}
+}
